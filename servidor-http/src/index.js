27190284151e670a0d932d7948a15fda5226dbcb
João Hugo Leda <joaohugoleda2@gmail.com>
2023-12-16 22:12:59 -0300

    routes

--- servidor-http/src/index.js
+++ servidor-http/src/index.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
+const routes = require('./routes')
+
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-type': 'text/html' });
-  response.end('<h1>Hello World</h1>');
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === request.url && routeObj.method === request.method
+  ));
+
+  if (route) {
+    route.handler(request, response);
+  } else {
+    response.writeHead(404, { 'Content-type': 'text/html' });
+    response.end(`Cannot ${request.method} ${request.url}`);
+  };
+
 });
 
 server.listen(3000, () => console.log('Server started at //localhost:3000'))
